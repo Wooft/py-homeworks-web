af40da444d812e77ac442606886097d1aea3c586
wooft <wooftyaa@gmail.com>
2023-03-25 19:27:32 +0300

    Complete

--- docker/Part 2/stocks_products/settings.py
+++ docker/Part 2/stocks_products/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nw^y+m^wmxza1asgk+)!ua2qx9)g+#v=6%76-9i8i(6eqiw94j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'netology_stocks_products',
         'USER': 'postgres',
         'PASSWORD': 'Shambala',
-        'HOST': 'postgredb',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
